ddda1c10475c1828bed488f965b7b31411f2fd66
KhrystychSerhii <khrystych.serhii.work@gmail.com>
2019-10-17 18:05:24 +0300

    ned

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -2,14 +2,7 @@ import * as firebase from 'firebase';
 
 // todo: move to .env
 const firebaseConfig = {
-    apiKey: 'AIzaSyDbAEB3eDEvY_PbwdII0UPtZQhbo4zkL_A',
-    authDomain: 'fir-react-43c53.firebaseapp.com',
-    databaseURL: 'https://fir-react-43c53.firebaseio.com',
-    projectId: 'fir-react-43c53',
-    storageBucket: 'gs://fir-react-43c53.appspot.com/',
-    messagingSenderId: '280069958396',
-    appId: '1:280069958396:web:1dffdb53431eb443b10579',
-    measurementId: 'G-M892SJY8EK'
+
 };
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
